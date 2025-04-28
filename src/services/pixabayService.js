7eab531a086ef1ea28af87347f8c0a3a34d31c2c
Trang Pham <trang.pham@workforceoptimizer.com>
2025-04-28 14:48:37 +0700

    fix icon

--- src/services/pixabayService.js
+++ src/services/pixabayService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '49979375-f07c8ad1079db1f51efd4c008'; // Replace with your API key
+const API_KEY = '49979375-f07c8ad1079db1f51efd4c008';
 const BASE_URL = 'https://pixabay.com/api/';
 
 export const fetchImages = async (page = 1, perPage = 20, query = '') => {
@@ -8,7 +8,7 @@ export const fetchImages = async (page = 1, perPage = 20, query = '') => {
     const response = await axios.get(BASE_URL, {
       params: {
         key: API_KEY,
-        q: query || 'nature', // Default search term
+        q: query || 'nature',
         page,
         per_page: perPage,
         image_type: 'photo',
